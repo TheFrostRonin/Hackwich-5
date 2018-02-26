e7f780662e52db47ae17b8da74525ef3bc95c135
CM Student <cmstudent@UHWO-ACM-C-05.local>
2018-02-26 10:51:12 -1000

    Finished Hackwich #5

--- Hackwich#5/ViewController.swift
+++ Hackwich#5/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource   {
+//define an array variable using an array literal with 5 items of type string
+    
+    @IBOutlet weak var tableView: UITableView!
+    var greatfriendArray = ["Kaito","Jeorge","Akane","Cole","Mei"]
+    
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//set VC as delgate
+        //self.tableview.datasource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return greatfriendArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //set up cell to display items in studentArray
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let text = greatfriendArray[indexPath.row]
+        cell.textLabel?.text = text
+        return cell
 
+    }
+    
+    
 }
 
